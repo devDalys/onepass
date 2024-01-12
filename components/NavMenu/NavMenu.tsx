3d83385a91b88e779d8f9434630d5c4744ea18b7
devDalys <idalys1357@gmail.com>
2024-01-13 01:36:03 +0300

    Редизайн меню

--- components/NavMenu/NavMenu.tsx
+++ components/NavMenu/NavMenu.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React from 'react';
 import styles from './NavMenu.module.scss';
 import Home from '@/assets/images/Home.svg';
@@ -5,23 +6,37 @@ import Add from '@/assets/images/Add.svg';
 import User from '@/assets/images/User.svg';
 import Link from 'next/link';
 import classNames from 'classnames';
+import {usePathname, useRouter} from 'next/navigation';
 
 interface NavMenuProps {
   onlyIsMobile?: boolean;
 }
 
 export default function NavMenu({onlyIsMobile = false}: NavMenuProps) {
+  const pathName = usePathname();
+
   return (
     <div className={classNames(styles.wrapper, {[styles.onlyMobile]: onlyIsMobile})}>
-      <Link className={styles.add} href={'/accounts/add'}>
+      <Link
+        className={classNames(styles.item, {[styles.active]: pathName === '/accounts/add'})}
+        href={'/accounts/add'}
+      >
         <Add className={styles.icon} />
         <span className={styles.text}>New account</span>
       </Link>
-      <Link className={styles.item} href={'/accounts'}>
+      <Link
+        className={classNames(styles.item, {
+          [styles.active]: pathName.startsWith('/accounts') && !pathName.includes('add'),
+        })}
+        href={'/accounts'}
+      >
         <Home className={styles.icon} />
         <span className={styles.text}>Accounts</span>
       </Link>
-      <Link className={styles.item} href={'/profile'}>
+      <Link
+        className={classNames(styles.item, {[styles.active]: pathName.startsWith('/profile')})}
+        href={'/profile'}
+      >
         <User className={styles.icon} />
         <span className={styles.text}>Profile</span>
       </Link>
