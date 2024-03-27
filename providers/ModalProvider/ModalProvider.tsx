d3bc03c94310ef3e6a13cf8bdf646100aa81a601
devDalys <idalys1357@gmail.com>
2024-03-28 00:26:56 +0300

    Немного оптимизации модалки

--- providers/ModalProvider/ModalProvider.tsx
+++ providers/ModalProvider/ModalProvider.tsx
@@ -17,29 +17,28 @@ export const ModalProvider = ({children}: {children: React.ReactNode}) => {
   const childrenRef = useRef<HTMLDivElement>(null);
   const [touchStart, setTouchStart] = useState(0);
   const [currentTop, setCurrentTop] = useState(0);
-  const handleDelete = () => {
+
+  const handleDelete = useCallback(() => {
     setModal(null);
     document.body.classList.remove(styles.hideScroll);
-  };
+  }, []);
 
-  const createModal = (props: CreateModalProps) => {
+  const createModal = useCallback((props: CreateModalProps) => {
     setModal(props);
     document.body.classList.add(styles.hideScroll);
-  };
+  }, []);
 
-  const hideWithAnimation = () => {
+  const hideWithAnimation = useCallback(() => {
     ref?.current?.classList.add(styles.closeAnimation);
-  };
+  }, []);
 
   const onTouch = useCallback(
     (event: React.TouchEvent<HTMLDivElement>) => {
       const translateTop = currentTop + Math.round(event.touches[0].clientY - touchStart);
       if (closeRef.current === event.target && ref.current && translateTop > 0) {
         ref.current.style.transform = `translate3d(0,${translateTop}px,0)`;
-
         if (childrenRef.current) {
           const height = window.innerHeight - childrenRef.current.getBoundingClientRect().top;
-          console.log(height);
           childrenRef.current.style.maxHeight = `${height}px`;
         }
       }
@@ -52,29 +51,24 @@ export const ModalProvider = ({children}: {children: React.ReactNode}) => {
       if (ref.current && closeRef.current === event.target) {
         const currentTop =
           ((event.changedTouches[0].clientY - touchStart) / window.innerHeight) * 100;
-        //Высчитывает сколько процентов экрана пролистано от НАЖАТИЯ до ОТПУСКАНИЯ
         ref?.current?.classList.remove(styles.stopTransition);
-
         if (currentTop <= -15) {
-          //Кейс когда потянули вверх больше чем на 15% viewport и раскрыть на фулл экран
-          childrenRef.current ? (childrenRef.current.style.maxHeight = '100%') : null;
+          childrenRef.current && (childrenRef.current.style.maxHeight = '100%');
           return (ref.current.style.transform = TRANSFORM_POSITIONS.fullTop);
         }
         if (currentTop >= 15) {
-          //Кейс когда потянули вниз больше чем на 15% viewport и нужно скрыть
           return hideWithAnimation();
         }
-        //Если недоскроллили сверху - вернуть вверх, иначе означает что скроллят по центру и вернуть в центр
         const isTopScrolling = (touchStart / window.innerHeight) * 100 < 15;
         if (isTopScrolling) {
           return (ref.current.style.transform = TRANSFORM_POSITIONS.fullTop);
         } else {
-          childrenRef.current ? (childrenRef.current.style.maxHeight = '35%') : null;
+          childrenRef.current && (childrenRef.current.style.maxHeight = '35%');
           return (ref.current.style.transform = TRANSFORM_POSITIONS.middle);
         }
       }
     },
-    [touchStart],
+    [touchStart, hideWithAnimation],
   );
 
   const onTouchStart = useCallback((event: React.TouchEvent<HTMLDivElement>) => {
@@ -83,9 +77,12 @@ export const ModalProvider = ({children}: {children: React.ReactNode}) => {
     ref?.current?.classList.add(styles.stopTransition);
   }, []);
 
-  const onAnimationEnd = useCallback((event: React.AnimationEvent) => {
-    return ANIMATION_NAMES.some((name) => event.animationName.includes(name)) && handleDelete();
-  }, []);
+  const onAnimationEnd = useCallback(
+    (event: React.AnimationEvent) => {
+      return ANIMATION_NAMES.some((name) => event.animationName.includes(name)) && handleDelete();
+    },
+    [handleDelete],
+  );
 
   return (
     <ModalContext.Provider value={{createModal, hideModal: hideWithAnimation}}>
